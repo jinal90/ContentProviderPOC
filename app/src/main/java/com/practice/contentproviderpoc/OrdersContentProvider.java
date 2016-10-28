093a04078a0b4b93b3fbb99470a3680b460e63f8
jinal90 <jinal010990@gmail.com>
2016-10-28 20:37:47 +0530

    content provider basic UriMatcher created

--- app/src/main/java/com/practice/contentproviderpoc/OrdersContentProvider.java
+++ app/src/main/java/com/practice/contentproviderpoc/OrdersContentProvider.java
@@ -4,6 +4,7 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -19,15 +20,6 @@ public class OrdersContentProvider extends ContentProvider {
     static final String URL = "content://" + PROVIDER_NAME + "/orders";
     static final Uri CONTENT_URI = Uri.parse(URL);
 
-    static final String _ID = "_id";
-    static final String TABLE_ID = "name";
-    static final String MENU_ITEM_ID = "grade";
-
-    private static HashMap<String, String> ORDERS_PROJECTION_MAP;
-
-    // database
-    private TableOrderManager database;
-
     static final int ORDERS = 1;
     static final int ORDER = 2;
     static final int RESTAURANT_TABLES = 3;
@@ -35,19 +27,20 @@ public class OrdersContentProvider extends ContentProvider {
     static final int MENU_ITEMS = 5;
     static final int MENU_ITEM = 6;
 
+    SQLiteDatabase db;
+    TableOrderManager dbHelper;
 
-
-    static final UriMatcher uriMatcher;
-    static{
+    private static UriMatcher uriMatcher;
+    static {
         uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-        uriMatcher.addURI(PROVIDER_NAME, "orders", ORDERS);
-        uriMatcher.addURI(PROVIDER_NAME, "orders/#", ORDER);
+        uriMatcher.addURI(PROVIDER_NAME, TableOrderManager.TABLE_NAME, ORDER);
+
     }
 
     @Override
     public boolean onCreate() {
-        database = new TableOrderManager(getContext());
-        return false;
+        dbHelper = new TableOrderManager(getContext());
+        return true;
     }
 
     @Nullable
@@ -67,6 +60,7 @@ public class OrdersContentProvider extends ContentProvider {
     public Uri insert(Uri uri, ContentValues values) {
         return null;
     }
+
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
